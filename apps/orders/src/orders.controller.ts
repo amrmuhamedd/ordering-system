689d90ab26c28de3eb1b7589f18e5fba3d686975
Amr Mohamed <amrmuhmedd@gmail.com>
2022-10-21 13:18:31 +0200

    finish create order and add validation

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { CreateOrderRequest } from './dto/create-order.dto';
 import { OrdersService } from './orders.service';
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  @Get()
-  getHello(): string {
-    return this.ordersService.getHello();
+  @Post()
+  async createOrder(@Body() request: CreateOrderRequest) {
+    return this.ordersService.createOrder(request);
   }
+
+
 }
