192a6405da936f0f1ecf88db7fd6b10ab6b3519b
Amr Mohamed <amrmuhmedd@gmail.com>
2022-10-23 07:30:31 +0200

    fix authentication error

--- apps/billing/src/billing.controller.ts
+++ apps/billing/src/billing.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { JwtAuthGuard } from '@app/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { EventPattern , Payload , RmqContext , Ctx} from '@nestjs/microservices';
 import { BillingService } from './billing.service';
 
 @Controller()
 export class BillingController {
   constructor(private readonly billingService: BillingService) {}
+  @UseGuards(JwtAuthGuard)
   @EventPattern('order_created')
   async handleOrderCreated(@Payload() data : any , @Ctx() context :  RmqContext){
     this.billingService.bill(data)
